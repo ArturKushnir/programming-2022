810e57dc627330206c591717a77118f0e6400a83
ArturKushnir <114510528+ArturKushnir@users.noreply.github.com>
2022-10-26 10:59:56 +0300

    Create homework_001.cpp

---
+++ Homeworks/homework_001.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int day = 0;
+	cout << "Enter the day" << day;
+	cin >> day;
+
+	int month = 0;
+	cout << "Enter the month" << month;
+	cin >> month;
+
+	if (day == 1)
+	{
+		cout << "January" << endl;
+	}
+
+	else if (day == 2)
+	{
+		cout << "February" << endl;
+	}
+
+}
+
